8e31a2e37f954e56fe788d10ca92644e20f317db
Admin Stagiaire <justinecoulon1@gmail.com>
2025-04-11 11:09:16 +0200

    added a placeholder when no book in shelf

--- src/components/profile/profile-section/shelves/shelf-card.tsx
+++ src/components/profile/profile-section/shelves/shelf-card.tsx
@@ -6,18 +6,23 @@ import classNames from 'classnames';
 import { Link } from '@/i18n/routing';
 import React from 'react';
 import CoverImage from '@/components/global/books/cover-image/cover-image';
+import { LibraryBig } from 'lucide-react';
 
 export default function ShelfCard({ shelf }: { shelf: ShelfWithLastBookDto }) {
   return (
     <Link href={`/profile/shelves/${shelf.id}`} className={classNames(styles.shelfCardContainer, 'nbShadow')}>
       <div className={styles.shelfImageContainer}>
-        {shelf.books.map((book) => (
-          <CoverImage
-            key={`shelf-book-cover-${book.isbn13}`}
-            isbn={book.isbn13}
-            className={styles.shelfBookCoverImage}
-          />
-        ))}
+        {shelf.books.length !== 0 ? (
+          shelf.books.map((book) => (
+            <CoverImage
+              key={`shelf-book-cover-${book.isbn13}`}
+              isbn={book.isbn13}
+              className={styles.shelfBookCoverImage}
+            />
+          ))
+        ) : (
+          <LibraryBig className={styles.shelfCoverImagePlaceholder} />
+        )}
       </div>
       <div className={styles.shelfNameContainer}>
         <h3 className={styles.shelfName}>{shelf.name}</h3>
